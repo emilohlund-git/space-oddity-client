7322e9156f96ece6b40adc89fdc7428b395c7692
Emilolund <emil@emilohlund.dev>
2023-07-19 11:32:26 +0200

    Fixing font issues

--- src/lib/components/card-back-logo.svelte
+++ src/lib/components/card-back-logo.svelte
@@ -1,5 +1,5 @@
 <div
-	class="flex pointer-events-none flex-col font-[AlienLeagueGradientBold] select-none justify-center text-lg items-center h-44 w-32 rounded-lg border-[1px] border-gray-700"
+	class="flex pointer-events-none flex-col font-alien-league-gradient-bold select-none justify-center text-lg items-center h-44 w-32 rounded-lg border-[1px] border-gray-700"
 >
 	<p class="text-red-500">Space</p>
 	<p class="text-blue-500">Oddity</p>
--- src/lib/components/logo-corner.svelte
+++ src/lib/components/logo-corner.svelte
@@ -1,5 +1,5 @@
 <div
-	class="font-[AlienLeagueGradient] mb-4 flex absolute top-10 left-10 flex-col text-[4rem] leading-[5rem]"
+	class="font-alien-league-gradient-bold mb-4 flex absolute top-10 left-10 flex-col text-[4rem] leading-[5rem]"
 >
 	<h1 class="text-red-500">Space</h1>
 	<h2 class="text-blue-500">Oddity</h2>
